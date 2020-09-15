ca1da8cb2262f0679e4c0043f3be336f559ae5ba
Aidan Gilmore <aidangilmore1@gmail.com>
2020-09-15 20:09:53 +0200

    initial workings

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [hex, setHex] = useState('#ffffff');
+  const randomizedHex = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    setHex(randomColor);
+  };
+
+  return (
+    <div className="App" style={{ backgroundColor: `${hex}`, minHeight: '33.34vh', overflow: 'hidden', zoom: '3' }}>
+      <h1> Random Color Generator </h1>
+      <h2> {hex} </h2>
+      <button onClick={randomizedHex}>Randomize</button>
+      <button onClick={() => navigator.clipboard.writeText(hex)} >
+        Copy Color
+    </button>
+
+
+    </div >
+  );
+};
+
+
+
+export default App;
